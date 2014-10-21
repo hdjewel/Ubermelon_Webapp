600bd91589b56757ee32393032db482eb5f2ea43
Hackbright Student <info@hackbrightacademy.com>
2014-10-20 18:16:28 -0700

    working logic to Task 4

--- melons.py
+++ melons.py
@@ -33,6 +33,19 @@ def shopping_cart():
     """TODO: Display the contents of the shopping cart. The shopping cart is a
     list held in the session that contains all the melons to be added. Check
     accompanying screenshots for details."""
+
+    # this is where we add the logic to get the qty from the "session cart"
+    #   that was "built" the "add_to_cart" function.
+    # then use the melon ids in the "cart" create a list of melons.  
+    # The melon details can be retrieved using the "model.get_melon_by_id".
+    # get_melon_by_id returns id, melon_type, common_name, price, imgurl,
+    #    flesh_color, rind_color, seedless
+    # Before passing the "melons" remember to calculate the "total price"
+    # and add it to the retrieved list of attributes.
+
+
+    # return render_template("cart.html", melons = melons)
+
     return render_template("cart.html")
 
 @app.route("/add_to_cart/<int:id>")
@@ -44,8 +57,41 @@ def add_to_cart(id):
     shopping cart page, while displaying the message
     "Successfully added to cart" """
 
-    return "Oops! This needs to be implemented!"
+     # add logicc here to populate the "Sessions dynamic database"
+     #  if "add ot cart button pressed " --- no if needed cause this is called 
+     #                                        when the button is pressed
+     #  if items exists in sessions 
+     #     if key exist in sessions
+     #         add 1 to quanty of the key
+     #     else
+     #        append "melon name as key: [qty = 1 , price]  ** calculate
+     #                             total before deplaying the cart page" 
+     #  otherwise
+     #     add "melon name key : [qty = 1, price] "    
+    
+    # the following lines print to the "terminal window"
+    print 0.0 , "+++++++++++++++++++++++++++++++++++"
+    print "id = ",
+    print id
+    print " value = ",
+    print session.get(id)
+    if "cart" in session:
+        if str(id) in session["cart"]:
+                # session[id][1] += 1
+            session["cart"][str(id)] += 1
+        else:
+            session["cart"][str(id)] = 1
+        print "###################################"
+        print session
+    else:
+        session["cart"] ={}
+        session["cart"][str(id)] = 1    
 
+    # return "Oops! This needs to be implemented!"
+    # return "Successfully added to cart! id = %r qty =%r" % (id, session[id])
+
+    #???flash "Successfully added to cart! id = %r qty =%r" % (id, session[id])
+    return redirect("/cart")
 
 @app.route("/login", methods=["GET"])
 def show_login():
